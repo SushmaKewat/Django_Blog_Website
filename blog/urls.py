8b5484b00a2bc9f0bc33ebf2f696b9d4cbaec99a
Sushma <sushmak1693@gmail.com>
2023-12-29 21:33:53 +0530

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .feeds import LatestPostsFeed
+from .views import *
+
+app_name = 'blog'
+
+urlpatterns = [
+    #post views
+    path('', post_list, name = 'post_list'),
+    path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+    #path('', PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         post_detail,
+         name = 'post_detail'),
+    path('<int:post_id>/share/', post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', post_search, name='post_search'),
+]
